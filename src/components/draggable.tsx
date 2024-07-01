f8f81781e3325d6bc85c2be3e2389aa960594204
Kevin Ostafinski <kevscripted@gmail.com>
2024-07-01 17:57:16 +0200

    installed react-use-draggable-scroll and created a Draggable wrapper

---
+++ src/components/draggable.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { ReactNode, useRef } from "react";
+import { useDraggable } from "react-use-draggable-scroll";
+
+const Draggable = ({ children }: { children: ReactNode }) => {
+  const dragRef =
+    useRef<HTMLDivElement>() as React.MutableRefObject<HTMLInputElement>;
+  const { events } = useDraggable(dragRef);
+
+  return (
+    <div
+      className={`w-full overflow-x-auto hide-scrollbar`}
+      ref={dragRef}
+      {...events}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Draggable;
