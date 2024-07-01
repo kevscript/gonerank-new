e4c35dc030a9be6d6b447b9316f579e78b9ad7ef
Kevin Ostafinski <kevscripted@gmail.com>
2024-07-01 23:07:15 +0200

    added href to TableCell

--- src/components/tables/table-cell.tsx
+++ src/components/tables/table-cell.tsx
@@ -1,13 +1,17 @@
+import Link from "next/link";
+
 type TableCellProps = {
   children: React.ReactNode;
   minRem?: number;
   align?: "left" | "center" | "right";
+  href?: string;
 };
 
 export function TableCell({
   children,
   minRem = 4,
   align = "center",
+  href,
 }: TableCellProps) {
   const alignClass =
     align === "right"
@@ -15,6 +19,19 @@ export function TableCell({
       : align === "left"
       ? "justify-start"
       : "justify-center";
+
+  if (href) {
+    return (
+      <Link href={href}>
+        <div
+          className={`h-full flex items-center px-4 ${alignClass}`}
+          style={{ minWidth: `${minRem}rem` }}
+        >
+          {children}
+        </div>
+      </Link>
+    );
+  }
 
   return (
     <div
