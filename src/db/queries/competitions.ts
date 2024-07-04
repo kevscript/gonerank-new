9bf5a5e03a281cd41147146ba01bdb3ae043fc7f
Kevin Ostafinski <kevscripted@gmail.com>
2024-07-04 15:44:40 +0200

    getAllCompetitions: updated query

--- src/db/queries/competitions.ts
+++ src/db/queries/competitions.ts
@@ -2,5 +2,13 @@ import { db } from "@/libs/kysely";
 import { unstable_cache } from "next/cache";
 
 export const getAllCompetitions = unstable_cache(async () => {
-  return db.selectFrom("competitions").selectAll().execute();
+  try {
+    const competitions = await db
+      .selectFrom("competitions")
+      .selectAll()
+      .execute();
+    return competitions;
+  } catch (err: any) {
+    throw new Error(err);
+  }
 }, ["competitions"]);
