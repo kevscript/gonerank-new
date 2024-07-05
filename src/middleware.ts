7b547f8261da0119ca03c81fa55753a5b1fb71e1
Kevin Ostafinski <kevscripted@gmail.com>
2024-07-05 23:28:48 +0200

    middleware: added temporary fix for mw causing server actions to return undefined

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,12 +9,22 @@ export function middleware(request: NextRequest) {
     "x-current-search-params",
     request.nextUrl.searchParams.toString()
   );
+
+  /* TEMP FIX FOR SERVER ACTIONS RETURNING UNDEFINED WHEN CUSTOM HEADERS ARE PASSED IN MIDDLEWARE */
+  if (process.env.NODE_ENV === "development") {
+    // this only occurs during development
+    if (headers.get("accept") === "text/x-component") {
+      headers.set("content-type", "text/x-component");
+    }
+  }
+  /**************/
+
   return NextResponse.next({ headers });
 }
 
-// export const config = {
-//   matcher: [
-//     // match all routes except static files and APIs
-//     "/((?!api|_next/static|_next/image|favicon.ico).*)",
-//   ],
-// };
+export const config = {
+  matcher: [
+    // match all routes except static files and APIs
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
