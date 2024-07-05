a852e4987401585f0ccf25a92b3fd0099dd333bb
Kevin Ostafinski <kevscripted@gmail.com>
2024-07-05 13:22:17 +0200

    updated popbar with useOutsideClick

--- src/components/popbar.tsx
+++ src/components/popbar.tsx
@@ -1,26 +1,29 @@
 "use client";
 
+import useOutsideClick from "@/hooks/useOutsideClick";
 import { usePopbar } from "@/hooks/usePopbar";
 import { PanelLeftClose } from "lucide-react";
+import { useRef } from "react";
 
 export function Popbar() {
   const { popStatus, handlePopStatus } = usePopbar();
-
-  function handlePopStatusOnCurrent(e: React.MouseEvent) {
-    if (e.currentTarget === e.target) {
-      handlePopStatus(popStatus === "close" ? "open" : "close");
+  const barRef = useRef(null);
+  function handleOutsideClick() {
+    if (popStatus === "open") {
+      handlePopStatus("close");
     }
   }
 
+  useOutsideClick({ ref: barRef, action: handleOutsideClick });
+
   return (
     <div
-      className={`z-50 w-full h-full bg-red-600/10 fixed lg:hidden ${
+      className={`z-50 w-full h-full fixed bg-gray-600/80 lg:hidden ${
         popStatus === "open" ? "block" : "hidden"
       }`}
-      onClick={(e) => handlePopStatusOnCurrent(e)}
     >
-      <div className="h-full w-64 bg-red-600 pr-[1px]">
-        <div className="w-full h-full bg-blue-600">
+      <div ref={barRef} className={`h-full w-64 bg-neutral-200 pr-[1px]`}>
+        <div className="w-full h-full bg-white">
           <div className="flex flex-col w-full h-full gap-8">
             <div className="w-full h-16 flex items-center px-4">
               <div className="flex flex-nowrap gap-4 h-full items-center">
@@ -28,10 +31,11 @@ export function Popbar() {
                   onClick={() => handlePopStatus("close")}
                   className="size-5"
                 />
-                <div className="size-10 flex justify-center items-center bg-neutral-100">
-                  <span className="font-black">GR</span>
-                </div>
               </div>
+            </div>
+
+            <div className="w-full h-16 bg-red-100">
+              <span>Navigation</span>
             </div>
           </div>
         </div>
